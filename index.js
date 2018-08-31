214a53947e3cc229256cfee6586538f5e02aebea
Sofía Maturana <sofimatsu@gmail.com>
2018-08-31 20:48:35 -0300

    promesa mdlinks

---
+++ index.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node 
+
+const mdLinks = require('./src/md-links').mdLinks;
+const path = require('path');
+const [, , ...args] = process.argv.slice(2);
+let options = {};
+if (args.includes('--validate')) options.validate = true;
+
+//Promesa sin la opción --validate
+mdLinks(path)
+	.then((links) => {
+		if (links.length === 0) console.error('No se encontraron enlaces');
+		links.forEach(element => {
+			let result = '';
+			result = element.path + ' : ' + element.line + ' : ' + element.href + ' : ' + element.text;
+			console.log(result);
+		});
+	}).catch((error) => {
+		console.error(error);
+	});
+//Promesa con la opción --validate
+mdLinks(path, { validate: true })
+	.then(links => links.forEach(element => {
+		let result = '';
+		if (options.validate) result = element.path + ' : ' + element.line + ' : ' + element.href + ' : ' + element.text + ' : ' + element.ok + ' : ' + element.status;
+		console.log(result);
+	})
+		.catch(console.error));
+
+module.exports = mdLinks;
